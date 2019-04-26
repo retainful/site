4445c2c3801b8438bb94ab65bfad277ee5790390
ShobanaEswaran <eswaranshobana@gmail.com>
2019-04-26 17:45:20 +0530

    - contact form

--- src/pages/support.js
+++ src/pages/support.js
@@ -8,9 +8,33 @@ const SupportPage = () => (
   <Layout>
     <SEO title="Support" />
     <div className="container">
-        <h1>Support</h1>
-        <p>Welcome to page 2</p>
-        <Link to="/">Go back to the homepage</Link>
+        <div className="contact-intro text-center">
+            <h3>Support</h3>
+            <p>Please fill-in and submit the form with your request. One of our support staff will be get in touch with you</p>
+        </div>
+        <div className="contact-form">
+            <form name="contact" method="POST" data-netlify="true" data-netlify-recaptcha="true">
+                <p>
+                    <label>Name: </label>
+                    <input type="text" name="name" placeholder="Enter your name" />
+                </p>
+                <p>
+                    <label>Email: </label>
+                    <input type="email" name="email" placeholder="name@name.com"  />
+                </p>
+                <p>
+                    <label>Subject: </label>
+                    <input type="text" name="name" placeholder="Subject" />
+                </p>
+                <p>
+                    <label>Message: </label>
+                    <textarea name="message" placeholder="Message" ></textarea>
+                </p>
+                <p>
+                    <button type="submit" className="btn btn-action btn-lg">Send</button>
+                </p>
+            </form>
+        </div>
     </div>
   </Layout>
 )
