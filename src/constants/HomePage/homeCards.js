2f11e25a3d29110e17eaf52e5a3f20f37021c1e1
ShobanaEswaran <eswaranshobana@gmail.com>
2019-04-29 16:03:08 +0530

    - Home page content updated

--- src/constants/HomePage/homeCards.js
+++ src/constants/HomePage/homeCards.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import bannerLogo from "../../images/retainful-coupon-03.png";
 
 const HomeCards = () =>(
     <div className="home-cards-container container text-center">
@@ -13,13 +12,13 @@ const HomeCards = () =>(
             <div className="col-md-4">
                 <div className="card-body">
                     <h4>Next order discount coupons</h4>
-                    <img className="img-fluid" src={`${process.env.GATSBY_RF_MEDIA_URL}/next_order_discouny_coupon.png`} alt="retainful-coupon-banner"/>
+                    <img className="img-fluid" src={`${process.env.GATSBY_RF_MEDIA_URL}/next_order_discouny_coupon.png`} alt="next_order_discouny_coupon"/>
                 </div>
             </div>
             <div className="col-md-4">
                 <div className="card-body">
                     <h4>Track the realtime conversions</h4>
-                    <img className="img-fluid" src={`${process.env.GATSBY_RF_MEDIA_URL}/track_realtime_conversions.png`} alt="retainful-coupon-banner"/>
+                    <img className="img-fluid" src={`${process.env.GATSBY_RF_MEDIA_URL}/track_realtime_conversions.png`} alt="track_realtime_conversions"/>
                 </div>
             </div>
         </div>
