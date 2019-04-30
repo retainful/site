6ab26224f61bd9347be280a986ea128d866ce77e
ShobanaEswaran <eswaranshobana@gmail.com>
2019-04-30 17:54:44 +0530

    - text changes in home and features

--- src/constants/HomePage/homeFeatures.js
+++ src/constants/HomePage/homeFeatures.js
@@ -19,21 +19,21 @@ const HomeFeatures = () =>(
                 <div className="col-sm-4">
                     <div className="features-list">
                         <h4>Automated Cart recovery email</h4>
-                        <p>Send cart recovery email reminders whenever a customer abandons the online cart. You can both schedule and automate series of timed cart recovery email reminders. No need to send emails manually. Also, email reminders are automatically stopped when the cart is recovered.</p>
+                        <p>Remind customers of their abandoned cart. Automatically schedule and send a sequence of email messages. No need to send emails manually. Unlimited emails can be sent.</p>
                     </div>
                     <div className="features-list">
-                        <h4>Unique Link to recover abandoned cart item</h4>
-                        <p>Add a unique recovery link along with cart recovery email. The customers can easily buy the items in the abandoned cart by just clicking on the recovery link.</p>
+                        <h4>Single-click cart recovery</h4>
+                        <p>Customers can recover the abandoned carts with a single click. The recovery link instantly takes the customer to the cart page with their abandoned items.</p>
                     </div>
                 </div>
                 <div className="col-sm-4">
                     <div className="features-list">
-                        <h4>Create Personalised Cart recovery emails</h4>
-                        <p>Use shortcodes to add customer details, cart summary and personalize all your cart recovery emails.</p>
+                        <h4>Personalised Cart recovery emails</h4>
+                        <p>Use shortcodes to personalize the emails. Add customer’s name, cart summary and product details. Personalised emails have a high conversion rates.</p>
                     </div>
                     <div className="features-list">
                         <h4>Reports and Insights</h4>
-                        <p>Get the extensive reports on the abandoned cart value, recovered cart value and also see the real-time cart of customers who are purchasing at your online store in the very moment.</p>
+                        <p>Track your email engagement. Compare the abandoned cart value and recovered cart value to assess the performance of your email campaigns. View real-time carts.</p>
                     </div>
                 </div>
             </div>
@@ -41,29 +41,29 @@ const HomeFeatures = () =>(
             <div className="row">
                 <div className="col-sm-4">
                     <div className="features-list">
-                        <h3>Next Order Discount Coupons</h3>
+                        <h3>Next Order Coupons</h3>
                         {/*<p>Retainful is the best tool to automate cart recovery emails and recover lost sales.</p>*/}
                         <p><img className="img-fluid" src={`${process.env.GATSBY_RF_MEDIA_URL}/next_order_discouny_coupon.png`} alt="next_order_discouny_coupon"/></p>
                     </div>
                 </div>
                 <div className="col-sm-4">
                     <div className="features-list">
-                        <h4>Unique Coupons</h4>
-                        <p>Include a unique coupon code for the next purchase in the order notification email itself. Order emails have a better open rate than promotional emails.</p>
+                        <h4>Unique Coupon codes</h4>
+                        <p>No more generic coupon codes. Retainful can automatically create and send unique coupon codes for the next purchases. You don’t need to juggle handling thousands of coupon codes.</p>
                     </div>
                     <div className="features-list">
-                        <h4>Percentage or Fixed</h4>
-                        <p>The Next order coupon can be a percentage coupon or a fixed value coupon. Create advanced coupon discounts by setting user and coupon conditions.</p>
+                        <h4>Automatic Coupon Code Generation</h4>
+                        <p>Next order coupons are generated automatically. The customers get the coupon for next order within his order confirmation email itself. The code is automatically included in the order notification email when customer places the order.</p>
                     </div>
                 </div>
                 <div className="col-sm-4">
                     <div className="features-list">
-                        <h4>Automatic Coupon Code Generation</h4>
-                        <p>Retainful generates the coupon codes instantly and emails it to the customers. You don’t have to use the same code for everyone & you don't have to juggle between thousands of coupon codes.</p>
+                        <h4>Percentage or Fixed</h4>
+                        <p>Coupons can be either a percentage or flat amount. Use advanced coupon settings to limit the usage of coupons. For example, you can let it apply only for products from certain categories.</p>
                     </div>
                     <div className="features-list">
                         <h4>Analytics & Insights</h4>
-                        <p>Get real-time insight into the performance of your emails. See how much revenue you got from repeat purchases with Retainful. Also, get a list of customers who got converted. Even you can see who have opened the emails that have a next purchase coupon code.</p>
+                        <p>Constant update on the revenue you made using the next order coupons. Actionable customer insights showing people who opened the email with a coupon, people who used coupon & got converted, and people who haven’t used the coupon code.</p>
                     </div>
                 </div>
             </div>
@@ -71,29 +71,29 @@ const HomeFeatures = () =>(
             <div className="row">
                 <div className="col-sm-4">
                     <div className="features-list">
-                        <h3>Track Real-Time Analytics & Insights</h3>
+                        <h3>Analytics & Insights</h3>
                         {/*<p>With Retainful, you get a featured dashboard to track analytics, conversions and customer insights.</p>*/}
                         <p><img className="img-fluid" src={`${process.env.GATSBY_RF_MEDIA_URL}/track_realtime_conversions.png`} alt="track_realtime_conversions"/></p>
                     </div>
                 </div>
                 <div className="col-sm-4">
                     <div className="features-list">
-                        <h4>Email Performance</h4>
-                        <p>Analyze how your next order & cart recovery email campaigns perform. Email metric like open rate and email click-through rate will help create better email campaigns.</p>
+                        <h4>Evaluate Email Performance</h4>
+                        <p>Track the performance of your cart recovery and coupon order emails. Email open rates and email click-through rates will help you know how many customers get engaged with your email campaign.</p>
                     </div>
                     <div className="features-list">
                         <h4>Track Sale Conversions</h4>
-                        <p>Track your WooCommerce sale conversions with useful analytics recovered carts and sales made with the next order coupons.</p>
+                        <p>Track the number of individual sale conversions achieved through next order coupons and cart recovery email campaigns.</p>
                     </div>
                 </div>
                 <div className="col-sm-4">
                     <div className="features-list">
                         <h4>Measure Revenue</h4>
-                        <p>Measure how much you earn. Get the detailed numbers on how much revenue is made from Cart recovery email campaign and Next order coupon code campaign.</p>
+                        <p>Get the detailed revenue report on the revenue made using cart recovery campaigns and Next order coupon campaigns.</p>
                     </div>
                     <div className="features-list">
-                        <h4>Use Customer Insights</h4>
-                        <p>Get to know the list of customers who opened emails & got converted. Also the list of customers who haven’t opened emails and haven’t used the coupon codes.</p>
+                        <h4>Actionable Customer Insights</h4>
+                        <p>Get actionable customer data. Create better email campaigns using the list of customers who got converted and the list of customers who still haven’t used the coupon.</p>
                     </div>
                 </div>
             </div>
