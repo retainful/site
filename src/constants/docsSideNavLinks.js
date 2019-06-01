2e043306519a3fc58adaf4e29316921ae64b620b
Ramesh Elamathi <rameshelamathi@gmail.com>
2019-06-01 15:39:35 +0530

    Updated doc navs

--- src/constants/docsSideNavLinks.js
+++ src/constants/docsSideNavLinks.js
@@ -10,6 +10,7 @@ const SIDENAV_LINKS = [
         LINKS: [
               {URL: '/docs/woocommerce/install-retainful-plugin-for-woocommerce', TEXTNODE: 'Installation'},
               {URL: '/docs/woocommerce/retainful-abandoned-cart-recovery', TEXTNODE: 'Abandoned Cart Recovery Emails'},
+              {URL: '/docs/woocommerce/collect-email-address-before-adding-to-cart-in-woocommerce', TEXTNODE: 'Email Collection during Add to Cart'},
               {URL: '/docs/woocommerce/next-order-coupon', TEXTNODE: 'Next Order Coupon'},
         ],
         BUTTON_LINK: '/docs/woocommerce/install-retainful-plugin-for-woocommerce',
