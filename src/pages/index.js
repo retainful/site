a4445e91420e5b5ac57da05ead12f9284c222eb3
rameshelamathi <rameshelamathi@gmail.com>
2019-07-02 20:10:14 +0530

    - Home page content is tweaked for better readability

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ import Logo from "../images/retainful-logo.png";
 
 const IndexPage = () => (
     <Layout className="home-page">
-        <SEO title="Abandoned Cart Recovery Emails for WooCommerce and Shopify | Retainful" description="Retainful helps you recover abandoned carts in your WooCommerce and Shopify stores. Automatically send a sequence of emails when customers abandon their carts. Drive repeat purchases by sending unique coupon codes for next orders"
+        <SEO title="Abandoned Cart Recovery Emails for WooCommerce and Shopify | Retainful" description="Recover abandoned carts and drive 10x more repeat purchases in your WooCommerce and Shopify stores. Automatically send a sequence of emails when customers abandon their carts. Drive repeat purchases by sending unique coupon codes for next orders"
              keywords={[`abandoned cart recovery`, `WooCommerce Abandoned Cart recovery emails`, `ecommerce customer retention`,`woocommerce followup emails`, `next order coupons`, `shopify abandoned cart recovery`, `shopify abandoned cart analytics`]}/>
 
         <HomeBanner/>
