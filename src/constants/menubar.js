56dd971fad9123114734309dda7b12f4fdf0f658
rameshelamathi <rameshelamathi@gmail.com>
2019-07-02 13:10:21 +0530

    - Reviews and menu bar content updated.

--- src/constants/menubar.js
+++ src/constants/menubar.js
@@ -7,14 +7,14 @@ const Menubar = [
             {
                 ICON: 'https://raw.githubusercontent.com/campaignrabbit/cr-media/master/images/home-icons/360-degree-view-of-customers.png',
                 TITLE: 'WooCommerce',
-                DESC: 'A complete payments platform engineered for growth.',
+                DESC:  'Download the Retainful Plugin for WooCommerce',
                 LINK: '/product/features/woocommerce',
 
             },
             {
                 ICON: 'https://raw.githubusercontent.com/campaignrabbit/cr-media/master/images/home-icons/follow-up-emails.png',
                 TITLE: 'Shopify',
-                DESC: 'A complete payments platform engineered for growth.',
+                DESC: 'Install the Shopify App and get started in minutes',
                 LINK: '/product/features/shopify',
             }
         ],
