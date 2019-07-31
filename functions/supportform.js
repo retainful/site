c56a4c29e5a222568820c0e7a57d2c20f5241bdd
shreegowtham27 <gforgowtham8@gmail.com>
2019-07-31 10:48:16 +0530

    Fix Added

--- functions/supportform.js
+++ functions/supportform.js
@@ -1,53 +1,52 @@
-// Gatsby uses weird config stuff
 require('dotenv').config({
-    path: `.env.${process.env}`,
-  })
-  
-  // Connect to our Mailgun API wrapper and instantiate it 
-  const mailgun = require('mailgun-js')
-  const mg = mailgun({
-    apiKey: process.env.MAILGUN_API_KEY,
-    domain: process.env.MAILGUN_DOMAIN,
-  })
-  
-  // Response stuff
-  const successCode = 200
-  const errorCode = 400
-  const headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Content-Type',
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+// Connect to our Mailgun API wrapper and instantiate it 
+const mailgun = require('mailgun-js')
+const mg = mailgun({
+  apiKey: process.env.MAILGUN_API_KEY,
+  domain: process.env.MAILGUN_DOMAIN,
+})
+
+// Response stuff
+const successCode = 200
+const errorCode = 400
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type',
+}
+
+// Our cloud function
+exports.handler = function(event, context, callback) {
+  let data = JSON.parse(event.body)
+  let { name, email, subject, message } = data
+  let mailOptions = {
+    from: `${name} <${email}>`,
+    to: process.env.MY_EMAIL_ADDRESS,
+    replyTo: email,
+    subject: `${subject}`,
+    text: `${message}`,
   }
   
-  // Our cloud function
-  exports.handler = function(event, context, callback) {
-    let data = JSON.parse(event.body)
-    let { name, email, subject, message } = data
-    let mailOptions = {
-      from: `${name} <${email}>`,
-      to: process.env.MY_EMAIL_ADDRESS,
-      replyTo: email,
-      subject: `${subject}`,
-      text: `${message}`,
+  // It's really as simple as this, 
+  // directly from the Mailgun dashboard
+  
+  mg.messages().send(mailOptions, (error, body) => {
+    if (error) {
+      console.log(error)
+      callback(null, {
+        errorCode,
+        headers,
+        body: JSON.stringify(error),
+      })
+    } else {
+      console.log(body)
+      callback(null, {
+        successCode,
+        headers,
+        body: JSON.stringify(body),
+      })
     }
-    
-    // It's really as simple as this, 
-    // directly from the Mailgun dashboard
-    
-    mg.messages().send(mailOptions, (error, body) => {
-      if (error) {
-        console.log(error)
-        callback(null, {
-          errorCode,
-          headers,
-          body: JSON.stringify(error),
-        })
-      } else {
-        console.log(body)
-        callback(null, {
-          successCode,
-          headers,
-          body: JSON.stringify(body),
-        })
-      }
-    })
-  }
+  })
+}
