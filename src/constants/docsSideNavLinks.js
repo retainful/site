eeea1f94e0be1d8de021f02c63b47f10000bf1ed
Ramesh Subramaniam <elamathi@Rameshs-MacBook-Pro.local>
2019-09-24 16:17:09 +0530

    Documentation for Exit Intent Popup added

--- src/constants/docsSideNavLinks.js
+++ src/constants/docsSideNavLinks.js
@@ -11,11 +11,12 @@ const SIDENAV_LINKS = [
               {URL: '/docs/woocommerce/install-retainful-plugin-for-woocommerce', TEXTNODE: 'Installation'},
               {URL: '/docs/woocommerce//retainful-upgrading-to-premium', TEXTNODE: 'Upgrade to Premium'},
               {URL: '/docs/woocommerce/retainful-abandoned-cart-recovery', TEXTNODE: 'Abandoned Cart Recovery Emails'},
+              {URL: '/docs/woocommerce/exit-intent-popup', TEXTNODE: 'Exit Intent Popup'},
               {URL: '/docs/woocommerce/collect-email-address-before-adding-to-cart-in-woocommerce', TEXTNODE: 'Email Collection Popup'},
               {URL: '/docs/woocommerce/coupon-for-email-popup', TEXTNODE: 'Coupon for Email Collection Popup'},
               {URL: '/docs/woocommerce/coupon-timer', TEXTNODE: 'Countdown Timer with Coupon Auto Apply'},
               {URL: '/docs/woocommerce/next-order-coupon', TEXTNODE: 'Next Order Coupon'},
-              {URL: '/docs/woocommerce/testing-next-order-coupons', TEXTNODE: 'Test Next order coupons'},
+              {URL: '/docs/woocommerce/testing-next-order-coupons', TEXTNODE: 'Testing your next order coupons'},
               {URL: '/docs/woocommerce/getting-premium-abandoned-cart-mail-templates', TEXTNODE: 'Getting premium Abandoned Cart email templates'},
 
         ],
