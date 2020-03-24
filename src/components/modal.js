c103c8fdb01afd1562f39f2e3875754955074611
shreegowtham27 <gforgowtham8@gmail.com>
2020-03-25 00:52:51 +0530

    Templates Page - Alpha

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,48 +1,48 @@
-import React, { Component } from 'react'
-import Modal from 'react-awesome-modal'
-import {FaWindowClose } from 'react-icons/fa'
+import React, { Component } from 'react';
+import Modal from 'react-awesome-modal';
+import {FaTimesCircle} from 'react-icons/fa'
 
 export default class Popup extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      visible: false,
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            visible : false
+        }
     }
-  }
 
-  openModal() {
-    this.setState({
-      visible: true,
-    })
-  }
+    openModal() {
+        this.setState({
+            visible : true
+        });
+    }
 
-  closeModal() {
-    this.setState({
-      visible: false,
-    })
-  }
+    closeModal() {
+        this.setState({
+            visible : false
+        });
+    }
 
-  render() {
-    // const properties = props
-    return (
-      <div style={{display:'flex',flexDirection:'column'}}>
-      <Modal visible={this.props.visible} effect="fadeInUp">
-        <a 
-        href="#"
-        onClick={this.props.onClickAway} 
-
-        style={{
-          float: 'right',
-          paddingRight:'10px',
-         }}
-      >
-        <FaWindowClose/>
-        </a>
-          <div className="text-center" style={{padding:'50px',fontWeight:'bold'}}>
-            {this.props.children}
-          </div>
-        </Modal>
-        </div>
-    )
-  }
+    render() {
+        // const properties = props
+        return (
+            <>
+            <span style={{cursor:'pointer'}} onClick={() => this.openModal()}> <div dangerouslySetInnerHTML={{__html: this.props.content}}/> </span>
+            <Modal 
+                visible={this.state.visible}
+                width="500"
+                height="350"
+                effect="fadeInUp"
+                onClickAway={() => this.closeModal()}
+            >
+                <div style={{padding:'10px'}}>
+                    <a href="javascript:void(0);" onClick={() => this.closeModal()} style={{float:'right'}}><FaTimesCircle/></a>
+                    <div style={{padding:'30px'}}>
+                      {this.props.children}
+                    </div>
+                </div>
+            </Modal>
+            </>
+        );
+    }
 }
