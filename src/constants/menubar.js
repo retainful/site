286fd5a9dafee760546370426a8c08962668274d
ShobanaEswaran <eswaranshobana@gmail.com>
2020-03-26 16:10:32 +0530

    Revamped megamenu

--- src/constants/menubar.js
+++ src/constants/menubar.js
@@ -5,37 +5,43 @@ const Menubar = [
         DROPDOWN_TARGET: 'featuresDropdown',
         DROPDOWN_LINKS: [
             {
-                ICON: 'https://raw.githubusercontent.com/retainful/site-images/master/menu-icons/woo-icon-logo.png',
-                TITLE: 'WooCommerce',
-                DESC:  'Learn how Retainful works with your WooCommerce Store',
-                LINK: '/product/features/woocommerce',
-
-            },
-            {
-                ICON: 'https://raw.githubusercontent.com/retainful/site-images/master/menu-icons/shopify-icon.png',
-                TITLE: 'Shopify',
-                DESC: 'Connect your Shopify store and start recovering lost sales with Retainful',
-                LINK: '/product/features/shopify',
-            },
-            {
-                ICON: 'https://raw.githubusercontent.com/retainful/site-images/master/menu-icons/icon.png',
-                TITLE: 'How it works',
-                DESC: 'Learn how Retainful works',
-                LINK: '/how-it-works',
-                SUB_DROPDOWN_MENU: true
+                MENUTITLE: 'Product'
             },
             {
                 ICON: 'https://raw.githubusercontent.com/retainful/site-images/master/menu-icons/1-04.jpg',
                 TITLE: 'Abandoned Cart Recovery',
                 LINK: '/abandoned-cart-recovery',
-                SUB_DROPDOWN_MENU: true
             },
             {
                 ICON: 'https://raw.githubusercontent.com/retainful/site-images/master/menu-icons/2-04.jpg',
                 TITLE: 'Next Order Coupon',
                 LINK: '/next-order-coupon',
-                SUB_DROPDOWN_MENU: true
-            }
+            },
+            {
+                ICON: 'https://raw.githubusercontent.com/retainful/site-images/master/menu-icons/icon.png',
+                TITLE: 'How it works',
+                LINK: '/how-it-works',
+            },
+            {
+                MENUTITLE: 'Integration',
+                SUB_DROPDOWN_MENU: true,
+                SUB_DROPDOWN_MENU_LINKS: [
+                    {
+                        ICON: 'https://raw.githubusercontent.com/retainful/site-images/master/menu-icons/woo-icon-logo.png',
+                        TITLE: 'WooCommerce',
+                        DESC:  'Learn how Retainful works with your WooCommerce Store',
+                        LINK: '/product/features/woocommerce',
+                        
+                    },
+                    {
+                        ICON: 'https://raw.githubusercontent.com/retainful/site-images/master/menu-icons/shopify-icon.png',
+                        TITLE: 'Shopify',
+                        DESC: 'Connect your Shopify store and start recovering lost sales with Retainful',
+                        LINK: '/product/features/shopify',
+                    },
+                ]
+            },
+            
         ],
         ISMEGAMENU: true
     },
--- src/pages/menucontent.js
+++ src/pages/menucontent.js
@@ -1,92 +1,130 @@
 import React from 'react'
-import {Link} from 'gatsby'
-import Menubar from '../constants/menubar';
-
+import { Link } from 'gatsby'
+import Menubar from '../constants/menubar'
 
 class MenuCard extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    componentDidMount() {
-        // console.log(this.props);
-    }
+  constructor(props) {
+    super(props)
+  }
+  componentDidMount() {
+    // console.log(this.props);
+  }
 
+  static menuCallout() {
+    return Menubar.map(card => {
+      const cardClass = `nav-link `
+      return (
+        <li className={'nav-item ' + (card.DROPDOWN_LINKS ? 'dropdown' : '')}>
+          {!card.DROPDOWN_LINKS && (
+            <Link activeClassName="active" to={card.LINK} className={cardClass}>
+              {card.TITLE}
+            </Link>
+          )}
+          {card.DROPDOWN_LINKS && (
+            <>
+              <a
+                className={
+                  'nav-link ' + (card.DROPDOWN_LINKS ? 'dropdown-toggle' : '')
+                }
+                href="#"
+                id={card.DROPDOWN_TARGET}
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                {card.TITLE}
+              </a>
+              <div
+                className={'dropdown-menu'}
+                aria-labelledby={card.DROPDOWN_TARGET}
+              >
+                {!card.ISMEGAMENU &&
+                  card.DROPDOWN_LINKS.map(link => {
+                    return (
+                      <a className="dropdown-item" href={link.LINK}>
+                        {link.TITLE}
+                      </a>
+                    )
+                  })}
 
-    static menuCallout() {
-        return Menubar.map((card) => {
-             const cardClass = `nav-link `;
-            return (
-                <li className={"nav-item " + (card.DROPDOWN_LINKS ? 'dropdown' : '')}>
-                    { !card.DROPDOWN_LINKS &&
-                        <Link activeClassName="active" to={card.LINK} className={cardClass}>{card.TITLE}</Link>
-                    }
-                    { card.DROPDOWN_LINKS &&
-                        <>
-                            <a className={"nav-link " + (card.DROPDOWN_LINKS ? 'dropdown-toggle' : '')} href="#" id={card.DROPDOWN_TARGET} role="button"
-                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                {card.TITLE}
-                            </a>
-                            <div className={"dropdown-menu"} aria-labelledby={card.DROPDOWN_TARGET}>
-                                { !card.ISMEGAMENU &&
-                                    card.DROPDOWN_LINKS.map((link) => {
-                                        return (
-                                            <a className="dropdown-item" href={link.LINK}>{link.TITLE}</a>
-                                        )
-                                    })
-                                }
-
-                                { card.ISMEGAMENU &&
-                                    <ul className="productsGroupPrimary">
+                {card.ISMEGAMENU && (
+                  <ul className="productsGroupPrimary">
+                    {card.DROPDOWN_LINKS.map(link => {
+                      if (link.MENUTITLE && !link.SUB_DROPDOWN_MENU) {
+                        return (
+                          <li>
+                            <h6 class="menu-title">{link.MENUTITLE}</h6>
+                          </li>
+                        )
+                      } else {
+                        if (link.SUB_DROPDOWN_MENU && link.MENUTITLE) {
+                            return (
+                                <li
+                                  className="submenu"
+                                >
+                                    <h6 class="menu-title">{link.MENUTITLE}</h6>
+                                    {link.SUB_DROPDOWN_MENU_LINKS &&
+                                        <ul>
                                         {
-                                    card.DROPDOWN_LINKS.map((link) => {
-                                                if(!link.SUB_DROPDOWN_MENU){
+                                            link.SUB_DROPDOWN_MENU_LINKS.map(sublink => {
                                                 return (
                                                     <li>
-                                                        <a className="linkContainer item-payments" href={link.LINK}
-                                                            data-analytics-action="payments" data-analytics-source="nav_dropdown" tabIndex="-1">
-                                                            <img src={link.ICON} alt=""/>
-                                                        <div className="productLinkContent">
-                                                                <h3 className="linkTitle">{link.TITLE}</h3>
-                                                            <p className="linkSub">{link.DESC}</p>
+                                                        <a
+                                                            className="linkContainer"
+                                                            href={sublink.LINK}
+                                                        >
+                                                            <img src={sublink.ICON} alt="" />
+                                                            <div className="productLinkContent">
+                                                                <h3 className="linkTitle">{sublink.TITLE}</h3>
+                                                                <p className="linkSub">{sublink.DESC}</p>
                                                             </div>
                                                         </a>
                                                     </li>
-                                                    )
-                                                }
-                                                else {
-                                                    return (
-                                                        <li className="submenu" style={{backgroundColor:'#f6f9fc'}}>
-                                                            <a className="linkContainer item-payments" href={link.LINK}
-                                                                data-analytics-action="payments" data-analytics-source="nav_dropdown" tabIndex="-1">
-                                                                <img src={link.ICON} alt=""/>
-                                                            <div className="productLinkContent">
-                                                                    <h3 className="linkTitle">{link.TITLE}</h3>
-                                                                <p className="linkSub">{link.DESC}</p>
-                                                                </div>
-                                                            </a>
-                                                        </li>
-                                                    )
-                                                }
+                                                )
                                             })
-                                        }
+                                            }
                                     </ul>
-                                }
+                                        }
+                                    
+                                </li>
+                              )
+                            
+                        } else {
+                            return (
+                                <li>
+                                  <a
+                                    className="linkContainer"
+                                    href={link.LINK}
+                                  >
+                                    <img src={link.ICON} alt="" />
+                                    <div className="productLinkContent">
+                                      <h3 className="linkTitle">{link.TITLE}</h3>
+                                      <p className="linkSub">{link.DESC}</p>
+                                    </div>
+                                  </a>
+                                </li>
+                              )
+                        }
+                      }
+                    })}
+                  </ul>
+                )}
+              </div>
+            </>
+          )}
+        </li>
+      )
+    })
+  }
 
-                            </div>
-                        </>
-                    }
-                </li>
-            );
-        });
-    }
-
-    render() {
-        return (
-         <ul className="navbar-nav ml-auto navbar-center">
-            {MenuCard.menuCallout()}
-        </ul>
-        )
-    }
+  render() {
+    return (
+      <ul className="navbar-nav ml-auto navbar-center">
+        {MenuCard.menuCallout()}
+      </ul>
+    )
+  }
 }
 
 export default MenuCard
