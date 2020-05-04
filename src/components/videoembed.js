9181599ebbf80b6f78f38111f8447a79bd1a6c0d
shreegowtham27 <gforgowtham8@gmail.com>
2020-05-04 13:12:47 +0530

    Icon spacing added iframe made responsive

--- src/components/videoembed.js
+++ src/components/videoembed.js
@@ -3,8 +3,10 @@ import React from "react"
 const Videoembed = (props) =>{
     const url= props.src
     return(
-        <iframe width="800" height="450" src={"https://www.youtube.com/embed/"+url+"?rel=0"} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="">
-        </iframe>
+        <div className="video-embed">
+            <iframe width="800" height="450" src={"https://www.youtube.com/embed/"+url+"?rel=0"} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="">
+            </iframe>
+        </div>
     )
 }
 
